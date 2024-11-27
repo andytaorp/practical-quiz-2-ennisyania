b4be1a26f0241d78b69f9b0d45c8806f832f7e5d
ennisyania <ennisyania@gmail.com>
2024-11-27 15:57:44 +0800

    completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,9 +5,36 @@ export default function Form({ onAddTask }) {
 
   const handleSubmit = (e) => {
     // TODO: write code to handle form submission
+      e.preventDefault();
+  
+      const task = {
+        id: Date.now(),
+        description: description,
+        completed: false,
+      };
+  
+      onAddTask(task);
+  
+      setDescription("");
+    
   };
 
-  return {
+  function handleDescription(e) {
+    setDescription(e.target.value);
+  }
+
+  return (
     /*TODO: add a form to add a new task*/
-  };
+    <form onSubmit={handleSubmit}>
+      <input
+        id="description"
+        type="text"
+        placeholder="Task Description..."
+        onChange={handleDescription}
+        value={description}
+      />
+
+      <button type="submit" onClick={handleSubmit}>Add</button>
+    </form>
+  );
 }
